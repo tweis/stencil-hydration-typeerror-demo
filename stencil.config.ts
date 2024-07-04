1aa8edb323a5459f8145dab713d18ef6f367090b
Tim Weisenberger <tim.weisenberger@netlogix.de>
2024-07-04 14:50:28 +0200

    feat: Add hydration server

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,4 +1,5 @@
 import { Config } from '@stencil/core';
+import { hydrateServerOutputTarget } from '@netlogix/stencil-hydrate-server';
 
 export const config: Config = {
   namespace: 'stencil-hydration-typeerror-demo',
@@ -19,6 +20,13 @@ export const config: Config = {
       type: 'www',
       serviceWorker: null, // disable service workers
     },
+    {
+      type: 'dist-hydrate-script',
+      dir: 'dist/hydrate',
+    },
+    hydrateServerOutputTarget({
+      dir: 'dist/ssr-server',
+    }),
   ],
   testing: {
     browserHeadless: "new",
